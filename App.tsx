2b7280551cea092a31fc327be631cca68e89f8a6
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-01 19:06:10 +0000

    #3 - Add basic navigation

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, StatusBar} from 'react-native';
+import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import FeedComponent from './src/components/FeedComponent';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
-      <NavigationContainer>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <>
-            <Text>Hello :D</Text>
-          </>
-        </SafeAreaView>
-      </NavigationContainer>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Feed ✨" component={FeedComponent} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
