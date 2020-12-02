9db5afc6f37031b906dd5ac1d7d8fd7e09d0f781
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-02 18:59:59 +0000

    #9 - Add fetch

--- src/components/FeedComponent.tsx
+++ src/components/FeedComponent.tsx
@@ -4,11 +4,38 @@ import FeedListItem from '../screens/FeedListItem';
 
 const FeedComponent = () => {
   const [dataProvider, setDataProvider] = useState(getDummyData());
+  const [isFetching, setIsFetching] = useState(false);
 
   const keyExtractor = (item: {}, index: number) => index.toString();
 
   const renderItem = ({item}) => {
     return <FeedListItem />;
+  };
+
+  const getData = () => {
+    setIsFetching(true);
+
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => {
+        setIsFetching(false);
+        //console.log('DEBUG data: ' + JSON.stringify(json));
+
+        console.log('DEBUG data: ' + JSON.stringify(dataProvider));
+        console.log(json);
+
+        setDataProvider(json.movies);
+
+        console.log('DEBUG data: ' + JSON.stringify(dataProvider));
+      })
+      .catch((error) => {
+        setIsFetching(false);
+        console.error(error);
+      });
+  };
+
+  const onRefresh = () => {
+    getData();
   };
 
   return (
@@ -17,6 +44,8 @@ const FeedComponent = () => {
         keyExtractor={keyExtractor}
         data={dataProvider}
         renderItem={renderItem}
+        onRefresh={() => onRefresh()}
+        refreshing={isFetching}
       />
     </View>
   );
