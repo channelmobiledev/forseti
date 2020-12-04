0d7bca0203735c179b5da3b3915535861a2de8db
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-04 19:32:18 +0000

    #12 - Seperate models into their own files

--- src/models/FeedItemModel.ts
+++ src/models/FeedItemModel.ts
@@ -1,15 +1,7 @@
+import Post from './PostModel';
+import User from './UserModel';
+
 export default interface FeedItemModel {
   user: User;
   post: Post;
-}
-
-interface User {
-  name: string;
-  avatar: string;
-}
-
-interface Post {
-  id: number;
-  title: string;
-  photo: string;
 }
