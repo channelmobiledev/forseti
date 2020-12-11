7db02e16491500a2577bb3ba584f559b1163a096
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-11 19:30:56 +0000

    #18 - Reorganize Profile Screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,78 +1,81 @@
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import {Avatar, Button, ListItem} from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import COLORS from '../constants/COLORS';
 
 const Tab = createMaterialTopTabNavigator();
 
 const ProfileScreen = () => {
   return (
-    <View style={{flex: 1, flexDirection: 'column'}}>
-      <ListItem>
-        <Avatar
-          rounded
-          size="xlarge"
-          source={{
-            uri: 'https://source.unsplash.com/random/800x600',
-          }}
-        />
-        <ListItem.Content>
-          <ListItem.Title>Example name</ListItem.Title>
-          <ListItem.Subtitle>20k subs</ListItem.Subtitle>
-          <Button
-            titleStyle={{
-              color: '#f14c48',
-            }}
-            buttonStyle={{borderColor: '#f14c48'}}
-            icon={
-              <MaterialCommunityIcons
-                name="crown-outline"
-                size={26}
-                color="#f14c48"
-              />
-            }
-            title="Follow"
-            type="outline"
-          />
-        </ListItem.Content>
-      </ListItem>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Following"
-          component={() => (
-            <View>
-              <Text>Hello</Text>
-            </View>
-          )}
-        />
-        <Tab.Screen
-          name="Followers"
-          component={() => (
-            <View>
-              <Text>Hello</Text>
-            </View>
-          )}
-        />
-        <Tab.Screen
-          name="Favourites"
-          component={() => (
-            <View>
-              <Text>Hello</Text>
-            </View>
-          )}
-        />
-        <Tab.Screen
-          name="Wishlist"
-          component={() => (
-            <View>
-              <Text>Hello</Text>
-            </View>
-          )}
-        />
-      </Tab.Navigator>
-    </View>
+    <ScrollView
+      contentContainerStyle={{flexGrow: 1}}
+      style={{flexDirection: 'column', backgroundColor: '#ff0000'}}>
+      <View style={{flex: 0}}>
+        <ProfileView />
+      </View>
+      <View style={{flex: 1}}>
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: '#ff073a',
+            indicatorStyle: {
+              backgroundColor: '#ff073a',
+            },
+          }}>
+          <Tab.Screen name="Following" component={DummyView} />
+          <Tab.Screen name="Followers" component={DummyView} />
+          <Tab.Screen name="Favourites" component={DummyView} />
+          <Tab.Screen name="Wishlist" component={DummyView} />
+        </Tab.Navigator>
+      </View>
+    </ScrollView>
   );
 };
 
+const ProfileView = () => {
+  return (
+    <ListItem containerStyle={{backgroundColor: COLORS.background}}>
+      <Avatar
+        rounded
+        size="xlarge"
+        source={{
+          uri: 'https://source.unsplash.com/random/800x600',
+        }}
+      />
+      <ListItem.Content>
+        <ListItem.Title style={{color: COLORS.text}}>
+          Example name
+        </ListItem.Title>
+        <ListItem.Subtitle style={{color: COLORS.text}}>
+          20k subs
+        </ListItem.Subtitle>
+        <Button
+          containerStyle={{marginTop: 10}}
+          titleStyle={{
+            color: '#ff073a',
+          }}
+          buttonStyle={{borderColor: '#ff073a'}}
+          icon={
+            <MaterialCommunityIcons
+              name="crown-outline"
+              size={26}
+              color="#ff073a"
+            />
+          }
+          title="Follow"
+          type="outline"
+        />
+      </ListItem.Content>
+    </ListItem>
+  );
+};
+
+const DummyView = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
 export default ProfileScreen;
