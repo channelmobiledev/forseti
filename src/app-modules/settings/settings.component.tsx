1465389934bc3b67a48c9fda5137b900c5fc824a
Channel Mobiledev <channel.mobiledev@gmail.com>
2021-01-22 19:46:34 +0000

    #28 - Add settings component

--- src/app-modules/settings/settings.component.tsx
+++ src/app-modules/settings/settings.component.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {AuthService} from '../../services/auth.service';
 import GradientView from '../utilityViews/gradient.screen';
 import SettingsScreen from './settings.screen';
 
+/**
+ * Services
+ */
+const authService: AuthService = new AuthService();
+
 const SettingsComponent = () => {
+  const profileLogout = () => {
+    authService.logout();
+  };
+
+  /**
+   * Render
+   */
   return (
     <GradientView>
-      <SettingsScreen />
+      <SettingsScreen onLogoutClick={() => profileLogout()} />
     </GradientView>
   );
 };
--- src/app-modules/settings/settings.screen.tsx
+++ src/app-modules/settings/settings.screen.tsx
@@ -1,11 +1,100 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, ScrollView, Text, View} from 'react-native';
+import {Button, ListItem} from 'react-native-elements';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import COLORS from '../../constants/COLORS';
 
-const SettingsScreen = () => {
+interface Props {
+  onLogoutClick: () => void;
+}
+
+/**
+ * Settings Screen
+ */
+const SettingsScreen = (props: Props) => {
+  /**
+   * Data models
+   */
+  interface ListItemSettingsViewType {
+    view: () => {};
+    onClick: () => void;
+  }
+
+  interface ListItemSettings {
+    name: string;
+    description: string;
+    type: ListItemSettingsViewType;
+  }
+
+  /**
+   * Generic Button Option view
+   */
+  const optionButtonConfirm = () => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignSelf: 'flex-end',
+        }}>
+        <Button
+          containerStyle={{marginTop: 10}}
+          titleStyle={{
+            color: COLORS.white,
+          }}
+          buttonStyle={{borderColor: COLORS.white}}
+          type="outline"
+          title={'Logout'}
+          icon={
+            <MaterialCommunityIcons
+              name="power"
+              size={20}
+              color={COLORS.white}
+            />
+          }
+          onPress={() => {
+            props.onLogoutClick();
+          }}></Button>
+      </View>
+    );
+  };
+
+  const settingsList: ListItemSettings[] = [
+    {
+      name: 'USERNAME',
+      description: 'Current user',
+      type: {view: optionButtonConfirm, onClick: () => {}},
+    },
+  ];
+
+  /**
+   * List item view
+   */
+  const settingListItem = ({item}: {item: ListItemSettings}) => {
+    /**
+     * Render
+     */
+    return (
+      <ListItem bottomDivider containerStyle={{backgroundColor: '#00000000'}}>
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+          <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+        </ListItem.Content>
+        <ListItem.Content>{item.type.view()}</ListItem.Content>
+      </ListItem>
+    );
+  };
+
+  /**
+   * Render
+   */
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <>
+      <FlatList
+        keyExtractor={(_, index) => index.toString()}
+        data={settingsList}
+        renderItem={settingListItem}
+      />
+    </>
   );
 };
 
